6d67693c6a0eb0e30ac3a40117528446a8bced08
georgerapeanu <alexandrurapeanu@yahoo.com>
2021-06-19 21:42:35 +0300

    Update installOpencv.sh

--- installOpencv.sh
+++ installOpencv.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
-cd ~/opencv
+cd ./opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=Release ../
